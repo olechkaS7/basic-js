8bb3b94cbbd217f23eb0c40f984d11a87cfcc488
olechkaS7 <olg.iurist@yandex.ru>
2020-03-11 15:17:42 +0300

    feat: add hanoi-tower task

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,7 +2,7 @@ module.exports = function countCats(arr) {
   let count = 0;
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
-      if (arr[i][j] === "^^")  count++;
+      if (arr[i][j] === "^^") count++;
     }
   }
   return count;
