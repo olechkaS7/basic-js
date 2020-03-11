36f4be60c71cd205430e15aa730b2825acfbb635
olechkaS7 <olg.iurist@yandex.ru>
2020-03-11 15:30:18 +0300

    feat: adjustment count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,7 @@
 module.exports = function countCats(arr) {
   let count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
+  for (let i = 0; i < arr.length; i+=1) {
+    for (let j = 0; j < arr[i].length; j+=1) {
       if (arr[i][j] === "^^") count++;
     }
   }
